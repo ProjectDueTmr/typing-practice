2650f458031c0abb01f9cc5d038120c06e7e3975
nick <nicksanchez2371@gmail.com>
2024-06-15 17:14:35 -0500

    shows random quote, and converts text into an array

---
+++ srcipt.js
@@ -0,0 +1,26 @@
+
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay') 
+const quoteInputElement = document.getElementById('quoteInput') 
+
+quoteInputElement.addEventListener('input', () => {
+    
+} )
+
+
+function getRandomQuote() {
+    return fetch('http://api.quotable.io/random').then(response => response.json()).then(data => data.content)
+}
+
+async function renderNewQuote(){
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerText = ''
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
+    quoteInputElement.value = null;
+}
+
+renderNewQuote()
